96386474272850357329c1945b077064a2ee0c31
Ludvík Prokopec <39367469+lewis-wow@users.noreply.github.com>
2022-01-27 23:12:08 +0100

    Better api

--- index.php
+++ index.php
@@ -4,6 +4,96 @@
  * DatabaseConnection
  */
 class DatabaseConnection extends PDO {
+    public function basicSelect($table, $what, $where = "", ...$params) {
+        $query = "SELECT $what FROM `$table` WHERE $where";
+        $prep = $this->prepare($query);
+        $prep->execute($params);
+
+        return $prep->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function select($table, $what, $where = "", ...$params) {
+        $res = $this->basicSelect($table, $what, $where, ...$params);
+        return !$res ? [] : $res;
+    }
+
+    public function selectAll($table, $what, $where = "", ...$params) {
+        $query = "SELECT $what FROM `$table` WHERE $where";
+        $prep = $this->prepare($query);
+        $prep->execute($params);
+
+        return $prep->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function selectAllIteratee($table, $what, $where, $params, $iteratee) {
+        $query = "SELECT $what FROM `$table` WHERE $where";
+        $prep = $this->prepare($query);
+        $prep->execute($params);
+
+        while ($row = $prep->fetch()) {
+            $iteratee($row, $prep);
+        }
+
+        return $prep;
+    }
+
+    public function delete($table, $where = "", ...$params) {
+        $query = "DELETE FROM `$table` WHERE $where";
+        $prep = $this->prepare($query);
+        $prep->execute($params);
+
+        return $prep;
+    }
+
+    public function update($table, $data, $where = "", ...$params) {
+        $dataKeys = array_keys($data);
+        $keys = "";
+        foreach ($dataKeys as $key) {
+            $keys .= "$key=:$key,";
+        }
+        $keys = substr($keys, 0, -1);
+        $query = "UPDATE `$table` SET $keys WHERE $where";
+
+        $prep = $this->prepare($query);
+        $prep->execute(array_merge($data, ...$params));
+
+        return $prep;
+    }
+
+    public function insert($table, $data) {
+        $dataKeys = array_keys($data);
+        $keys = implode(",", $dataKeys);
+        $valuesHooks = ":" . implode(", :", $dataKeys);
+        $query = "INSERT INTO `$table` ($keys) VALUES ($valuesHooks)";
+
+        $prep = $this->prepare($query);
+        $prep->execute($data);
+
+        return $prep;
+    }
+
+    public function overwrite($table, $data, $where = "", ...$params) {
+        $res = $this->select($table, "*", $where, ...$data);
+
+        if ($res && $res->num_rows > 0) return $this->update($table, $data, $where, ...$params);
+
+        return $this->insert($table, $data);
+    }
+
+    public function safeQuery($q, ...$params) {
+        $prep = $this->prepare($q);
+        $prep->execute($params);
+
+        return $prep;
+    }
+
+    public function run($table, $what, $where = "", ...$params) {
+        $query = "SELECT $what FROM `$table` WHERE $where";
+        $prep = $this->prepare($query);
+        $prep->execute($params);
+
+        return $prep;
+    }
 }
 
 class DatabaseTable {
@@ -25,118 +115,39 @@ class DatabaseTableUtils extends DatabaseTable {
     protected $table;
 
     public function select($what, $where, ...$params) {
-        $query = "SELECT $what FROM `$this->table` WHERE $where";
-        $prep = $this->db->prepare($query);
-
-        if (!$this->on_query($prep)) return self::TERMINATED;
-
-        $res = $prep->execute($params);
-        if (!$res) return self::FAIL;
-        if ($prep->rowCount() == 0) return self::EMPTY;
-
-        return $prep->fetch(PDO::FETCH_ASSOC);
+        return $this->db->select($this->table, $what, $where, ...$params);
     }
 
     public function selectAll($what, $where, ...$params) {
-        $query = "SELECT $what FROM `$this->table` WHERE $where";
-        $prep = $this->db->prepare($query);
-
-        if (!$this->on_query($prep)) return self::TERMINATED;
-
-        $res = $prep->execute($params);
-        if (!$res) return self::FAIL;
-        if ($prep->rowCount() == 0) return self::EMPTY;
-
-        return $prep->fetchAll(PDO::FETCH_ASSOC);
+        return $this->db->selectAll($this->table, $what, $where, ...$params);
     }
 
     public function selectAllIteratee($what, $where, $params, $iteratee) {
-        $query = "SELECT $what FROM `$this->table` WHERE $where";
-        $prep = $this->db->prepare($query);
-
-        if (!$this->on_query($prep)) return self::TERMINATED;
-
-        $res = $prep->execute($params);
-        if (!$res) return self::FAIL;
-
-        while ($row = $prep->fetch()) {
-            $iteratee($row, $prep);
-        }
-
-        return $prep;
+        return $this->db->selectAllIteratee($this->table, $what, $where, $params, $iteratee);
     }
 
     public function delete($where, ...$params) {
-        $query = "DELETE FROM `$this->table` WHERE $where";
-        $prep = $this->db->prepare($query);
-
-        if (!$this->on_query($prep)) return self::TERMINATED;
-
-        $res = $prep->execute($params);
-        if (!$res) return self::FAIL;
-
-        return $prep;
+        return $this->db->delete($this->table, $where, ...$params);
     }
 
     public function update($data, $where, ...$params) {
-        $dataKeys = array_keys($data);
-        $keys = "";
-        foreach ($dataKeys as $key) {
-            $keys .= "$key=:$key,";
-        }
-        $keys = substr($keys, 0, -1);
-        $query = "UPDATE `$this->table` SET $keys WHERE $where";
-
-        $prep = $this->db->prepare($query);
-
-        if (!$this->on_query($prep)) return self::TERMINATED;
-
-        $res = $prep->execute(array_merge($data, ...$params));
-        if (!$res) return self::FAIL;
-
-        return $prep;
+        return $this->db->update($this->table, $data, $where, ...$params);
     }
 
     public function insert($data) {
-        $dataKeys = array_keys($data);
-        $keys = implode(",", $dataKeys);
-        $valuesHooks = ":" . implode(", :", $dataKeys);
-        $query = "INSERT INTO `$this->table` ($keys) VALUES ($valuesHooks)";
-
-        $prep = $this->db->prepare($query);
-
-        if (!$this->on_query($prep)) return self::TERMINATED;
-
-        $res = $prep->execute($data);
-        if (!$res) return self::FAIL;
-
-        return $prep;
+        return $this->db->insert($this->table, $data);
     }
 
     public function overwrite($data, $where, ...$params) {
-        $res = $this->select("*", $where, ...$data);
-
-        if ($res && $res->num_rows > 0) return $this->update($data, $where, ...$params);
-
-        return $this->insert($data);
+        return $this->db->overwrite($this->table, $data, $where, ...$params);
     }
 
-    public function query($q, ...$params) {
-        $prep = $this->db->prepare($q);
-
-        if (!$this->on_query($prep)) return self::TERMINATED;
-
-        if (!$prep->execute($params)) return false;
-
-        return $prep;
+    public function query($what, $where, ...$params) {
+        return $this->db->run($this->table, $what, $where, ...$params);
     }
 
     public function lastInsertId() {
         return $this->db->lastInsertId();
-    }
-
-    public function on_query($q) {
-        return true;
     }
 
     public function close() {
@@ -146,14 +157,3 @@ class DatabaseTableUtils extends DatabaseTable {
         return null;
     }
 }
-
-/**
- * usage
- */
-
-/** users.php file */
-return DatabaseTable::connect($db, "users", new class extends DatabaseTableUtils {
-    public function login($email, $password) {
-        return $this->select("*", "email=? AND password=?", $email, $password);
-    }
-});
